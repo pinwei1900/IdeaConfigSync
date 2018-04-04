1190ba0e529e33c76ebcf42400bab0eb32316b77
haosonglin <1124106217@qq.com>
2018-04-04 11:49:41 +0800

    IU-2017.2.5 <lin@DESKTOP-MQ521AM Create markdown.xml, IntelliLang.xml, print.xml, project.default.xml, diff.xml, Default copy.xml, Default.xml, applicationLibraries.xml, find.xml, File Header.java, keymap.xml, gradle.run.settings.xml, code.style.schemes.xml, _@user_Darcula.icls, github_settings.xml, mavenVersion.xml, Default.xml, debugger.renderers.xml, copyright.java, editor.xml, debugger.xml, baseRefactoring.xml, vim_settings.xml, ide.general.xml, git.xml, other.xml, GoogleStyle.xml, ActionScript File Header.as, ui.lnf.xml, lombok.xml, filetypes.xml, projectView.xml, javaeeExternalResources.xml, perl5.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, consoleFolding.xml, laf.xml, parameter.hints.xml, Class.java, jdk.table.xml, path.macros.xml, intentionSettings.xml, web-browsers.xml, proxy.settings.xml, dbnavigator.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml

---
+++ fileTemplates/includes/ActionScript File Header.as
@@ -0,0 +1,7 @@
+/**
+* @Description 
+* @Author <a href="mailto:haosonglin@xwchina.com ">songlin.Hao</a>
+* @Date ${DATE}
+* @Version 1.0.0
+*/
+
---
+++ fileTemplates/includes/copyright.java
@@ -0,0 +1,4 @@
+/*
+ * Copyright (c) ${YEAR}年${MONTH}月${DAY}日 by XuanWu Wireless Technology Co.Ltd. 
+ *             All rights reserved                         
+ */
